5c263b81a9b078883098a44a467ac224fd531bea
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-08-22 19:22:26 -0400

    Add content to and style modal

--- src/renderUI.js
+++ src/renderUI.js
@@ -32,7 +32,7 @@ const renderUI = () => {
   prjctModal.appendChild(prjModalPara);
 
   const prjModalLbl = document.createElement("label");
-  prjModalLbl.setAttribute("for", "name");
+  prjModalLbl.setAttribute("for", "project-name");
   prjModalPara.appendChild(prjModalLbl);
 
   const prjModalInput = document.createElement("input");
@@ -41,6 +41,20 @@ const renderUI = () => {
   prjModalInput.setAttribute("placeholder", "Project Name...");
   prjModalInput.required = true;
   prjModalPara.appendChild(prjModalInput);
+
+  const btnContainer = document.createElement("div");
+  btnContainer.classList.add("btn-container");
+  prjctModal.appendChild(btnContainer);
+
+  const addNewPrjctBtn = document.createElement("button");
+  addNewPrjctBtn.setAttribute("id", "add-new-project");
+  addNewPrjctBtn.innerText = "Add";
+  btnContainer.appendChild(addNewPrjctBtn);
+
+  const cnclNewPrjctBtn = document.createElement("button");
+  cnclNewPrjctBtn.setAttribute("id", "cncl-new-project");
+  cnclNewPrjctBtn.innerText = "Cancel";
+  btnContainer.appendChild(cnclNewPrjctBtn);
 
   const addPrjctBtn = document.createElement("button");
   addPrjctBtn.setAttribute("id", "add-project");
